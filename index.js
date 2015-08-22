8268acec503c09bd707137490edfd010be7f0a64
Ae <hi@angel-estrada.com>
2015-08-22 18:34:11 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports            = require('gsap/src/uncompressed/TweenMax');
+module.exports.Tween      = require('gsap/src/uncompressed/TweenMax');
+module.exports.Draggable  = require('gsap/src/uncompressed/utils/Draggable');
+module.exports.ThrowProps = require('gsap/src/uncompressed/utils/ThrowProps');
