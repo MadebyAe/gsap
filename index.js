d6b08422d5297421890287c03ad4ec051ddb063d
Ae <hi@angel-estrada.com>
2015-08-22 19:17:34 -0500

    Add throwprops

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports            = require('gsap/src/uncompressed/TweenMax');
-module.exports.Tween      = require('gsap/src/uncompressed/TweenMax');
-module.exports.Draggable  = require('gsap/src/uncompressed/utils/Draggable');
-module.exports.ThrowProps = require('gsap/src/uncompressed/utils/ThrowProps');
+module.exports            = require('./src/uncompressed/TweenMax');
+module.exports.Tween      = require('./src/uncompressed/TweenMax');
+module.exports.Draggable  = require('./src/uncompressed/utils/Draggable');
+module.exports.ThrowProps = require('./src/uncompressed/utils/ThrowProps');
